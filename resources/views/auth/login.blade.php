ed38e37dbb6a841ec783f4ed1cec6b0bb1681219
jsoliz064 <jsoliz064@gmail.com>
2021-12-11 15:43:47 -0400

    ga

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Solid - Bootstrap Business Template</title>
+  <title>CRM - Ingresar</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
@@ -70,7 +70,7 @@
  
     
 
-
+<br>
     <div class="headerwrap2">
         <div class="container">
             <div class="col-lg-8 col-lg-offset-2">
